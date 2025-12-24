f78efb3faa77573cc7dc095f118330c68562defe
andre-ads1 <andre.araujo7@aluno.unip.br>
2025-12-23 21:37:46 -0300

    corrige a atribuição da variável chute e organiza a estrutura do código

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,3 +1,7 @@
 alert ("Boas vindas ao jogo do número secreto!");
 let numeroSecreto = 29;
-prompt("Digite um número entre 0 e 100:");
+let chute =prompt("Digite um número entre 0 e 100:");
+
+if (chute == numeroSecreto) {
+    console.log("Você acertou! Parabéns!");
+}
