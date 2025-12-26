67557eeb41855c2e26660efc8369b974d3cee074
andre-ads1 <andre.araujo7@aluno.unip.br>
2025-12-26 13:33:10 -0300

    corrige o número secreto e altera a mensagem de acerto para um alerta

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,7 +1,11 @@
 alert ("Boas vindas ao jogo do número secreto!");
-let numeroSecreto = 29;
+let numeroSecreto = 5;
+ console.log("Número secreto é: " + numeroSecreto);
 let chute =prompt("Digite um número entre 0 e 100:");
 
+// se chute for igual ao número secreto
 if (chute == numeroSecreto) {
-    console.log("Você acertou! Parabéns!");
+    alert("Você acertou! Parabéns!");
+} else {
+    alert("Você errou! Tente novamente!");
 }
