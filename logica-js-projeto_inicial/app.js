5344b41cf3036b3e16df8a45527391f68cf56da1
andre-ads1 <andre.araujo7@aluno.unip.br>
2025-12-27 20:41:03 -0300

    refatora a lógica do jogo para permitir múltiplas tentativas e corrige a mensagem de acerto

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,11 +1,22 @@
 alert ("Boas vindas ao jogo do número secreto!");
 let numeroSecreto = 5;
  console.log("Número secreto é: " + numeroSecreto);
-let chute =prompt("Digite um número entre 0 e 100:");
+let chute;
+let tentativas = 1;
 
-// se chute for igual ao número secreto
-if (chute == numeroSecreto) {
-    alert(`Você acertou! Parabéns, Você descobriu o número secreto! ${numeroSecreto}`);
-} else {
-    alert("Você errou! Tente novamente!");
+//enquanto chute não for igual ao número secreto
+while (chute != numeroSecreto) {
+    chute = prompt("Digite um número entre 0 e 10:");
+    // se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        alert(`Você acertou! Parabéns, Você descobriu o número secreto! ${numeroSecreto} com ${tentativas} tentativas.`);
+    } else {
+        if (chute > numeroSecreto) {
+        alert(`O número secreto é menor que ${chute}`);
+        } else {
+        alert(`O número secreto é maior que ${chute}`);
+        }
+        tentativas = tentativas + 1;
+        tentativas++;
+    }
 }
